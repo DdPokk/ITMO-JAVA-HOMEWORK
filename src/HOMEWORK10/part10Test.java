05effe68e31cad5597c94cf51fd33c22f268baf8
DmDm <9111322054@mail.ru>
2022-04-28 16:20:39 +0300

    Обновлено

--- src/HOMEWORK10/part10Test.java
+++ src/HOMEWORK10/part10Test.java
@@ -11,20 +11,20 @@ public class part10Test {
         File text3 = new File("files/text3.txt");
         File text5 = new File("files/text5.txt");
 
-        System.out.println("пункт1");
+        System.out.println("?????1");
         List fileList = part10.fileToStringList(file1);
         fileList.forEach(System.out::println);
 
-        System.out.println("пункт2");
+        System.out.println("?????2");
         part10.text3("aaaaaaaaabbbbbbbbbbbcccccccccc", text3);
-        System.out.println("Смотри файл text3.txt");
+        System.out.println("?????? ???? text3.txt");
 
-        System.out.println("пункт3");
+        System.out.println("?????3");
         part10.text4(file1, file2,text4);
-        System.out.println("Смотри файл text4.txt");
+        System.out.println("?????? ???? text4.txt");
 
-        System.out.println("пункт4");
+        System.out.println("?????4");
         part10.text5(text5);
-        System.out.println("Смотри файл text5.txt");
+        System.out.println("?????? ???? text5.txt");
     }
 }
